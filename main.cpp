b74d3d940c34440b14ed6670c6c1d4bd074ea869
Gabriel Sizilio <gabrielsizilio@gmail.com>
2024-09-06 12:29:10 -0300

    feat: add pause game

--- main.cpp
+++ main.cpp
@@ -41,39 +41,43 @@ float ballYSpeed = 5.0f;
 int score1 = 0;
 int score2 = 0;
 
-void setDifficulty(Difficulty difficulty)
-{
-    switch (difficulty)
-    {
-    case EASY:
-        ballXSpeed = 3.0f;
-        ballYSpeed = 3.0f;
-        barHeight = 150.0f;
-        break;
-    case MEDIUM:
-        ballXSpeed = 5.0f;
-        ballYSpeed = 5.0f;
-        barHeight = 100.0f;
-        break;
-    case HARD:
-        ballXSpeed = 8.0f;
-        ballYSpeed = 8.0f;
-        barHeight = 50.0f;
-        break;
+// Estado do jogo
+bool endend = false;
+bool paused = false;
 
-    default:
-        break;
+void restartGame();
+
+void handleMouseClick(int button, int state, int x, int y)
+{
+    if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
+    {
+        paused = true;
     }
 }
 
 void handleKeysDown(unsigned char key, int x, int y)
 {
-    keyStates[key] = true; // Marca a tecla como pressionada
+    if (key == 'R' || key == 'r')
+    {
+        restartGame();
+    }
+
+    if (key == 'M' || key == 'm')
+    {
+        paused = !paused;
+    }
+    else
+    {
+        keyStates[key] = true; // Marca a tecla como pressionada
+    }
 }
 
 void handleKeysUp(unsigned char key, int x, int y)
 {
-    keyStates[key] = false; // Marca a tecla como liberada
+    if (key != 'M' || key != 'm')
+    {
+        keyStates[key] = false; // Marca a tecla como liberada
+    }
 }
 
 void handleKeysDownSpecial(int key, int x, int y)
@@ -96,7 +100,7 @@ void playerLeft()
     glEnd();
 }
 
-void playerRight(int barHeight)
+void playerRight()
 {
     glBegin(GL_QUADS);
     glVertex2f(width, bar2Y);
@@ -120,6 +124,13 @@ void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
 
+    const char *info = "Click right button to change game difficuty";
+    glRasterPos2f(width / 2 - 120, height - 25);
+    for (const char *c = info; *c != '\0'; c++)
+    {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_10, *c);
+    }
+
     // Desenha o placar
     glRasterPos2f(width / 2 - 50, height - 50);
     glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, '0' + score1);
@@ -141,19 +152,92 @@ void display()
 
     // BARRA-RIGHT
     glPushMatrix();
-    playerRight(barHeight);
+    playerRight();
     glPopMatrix();
 
     glutSwapBuffers();
 }
 
+void restartGame()
+{
+    bar1Y = height / 2 - barHeight / 2;
+    bar2Y = height / 2 - barHeight / 2;
+
+    ballX = width / 2;
+    ballY = height / 2;
+
+    score1 = 0;
+    score2 = 0;
+
+    paused = false;
+}
+
+void setDifficulty(Difficulty difficulty)
+{
+    switch (difficulty)
+    {
+    case EASY:
+        ballXSpeed = 3.0f;
+        ballYSpeed = 3.0f;
+        barHeight = 150.0f;
+        break;
+    case MEDIUM:
+        ballXSpeed = 5.0f;
+        ballYSpeed = 5.0f;
+        barHeight = 100.0f;
+        break;
+    case HARD:
+        ballXSpeed = 8.0f;
+        ballYSpeed = 8.0f;
+        barHeight = 50.0f;
+        break;
+    }
+
+    restartGame();
+}
+
+void difficutyMenu(int option)
+{
+    paused = true;
+    switch (option)
+    {
+    case EASY:
+        setDifficulty(EASY);
+        break;
+
+    case MEDIUM:
+        setDifficulty(MEDIUM);
+        break;
+
+    case HARD:
+        setDifficulty(HARD);
+        break;
+    }
+}
+
+void displayMenu()
+{
+    int menu = glutCreateMenu(difficutyMenu);
+    glutAddMenuEntry("EASY", EASY);
+    glutAddMenuEntry("MEDIUM", MEDIUM);
+    glutAddMenuEntry("HARD", HARD);
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
+}
+
 void dislpayWinner()
 {
     const char *winner = (score1 == WINNERSCORE) ? "Player left win!" : "Player right win!";
+    const char *restart = "Press R to restart game";
 
     // Definindo a posição onde a mensagem será exibida
-    glRasterPos2f(width / 2 - 50, height - 100);
+    glRasterPos2f(width / 2 - 80, height - 150);
     for (const char *c = winner; *c != '\0'; c++)
+    {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, *c);
+    }
+
+    glRasterPos2f(width / 2 - 115, height - 175);
+    for (const char *c = restart; *c != '\0'; c++)
     {
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, *c);
     }
@@ -163,21 +247,24 @@ void dislpayWinner()
 
 void update(int value)
 {
+    // Pausa o game
+    if (paused)
+    {
+        glutTimerFunc(16, update, 0);
+        return;
+    }
+
+    if (score1 == WINNERSCORE || score2 == WINNERSCORE)
+    {
+        dislpayWinner();
+        paused = true;
+        glutTimerFunc(16, update, 0);
+        return;
+    }
+
     // Lógica de atualização do jogo, incluindo movimentação das barras e bola
     ballX += ballXSpeed;
     ballY += ballYSpeed;
-
-    if (score1 == WINNERSCORE)
-    {
-        dislpayWinner();
-        return;
-    }
-
-    if (score2 == WINNERSCORE)
-    {
-        dislpayWinner();
-        return;
-    }
 
     // Espaço para a lógica de movimentação das barras
     if ((keyStates['w'] || keyStates['W']) && (bar1Y + barHeight < height))
@@ -298,13 +385,15 @@ int main(int argc, char **argv)
 {
     Init(argc, argv);
     glutDisplayFunc(display);
+    displayMenu();
     glutTimerFunc(16, update, 0);
+    glutMouseFunc(handleMouseClick);
     glutKeyboardFunc(handleKeysDown);
     glutKeyboardUpFunc(handleKeysUp);
     glutSpecialFunc(handleKeysDownSpecial);
     glutSpecialUpFunc(handleKeysUpSpecial);
     glutReshapeFunc(reshape);
-    setDifficulty(HARD);
+    setDifficulty(MEDIUM);
 
     glutMainLoop();
     return 0;
